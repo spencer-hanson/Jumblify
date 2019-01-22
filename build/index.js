918373447786b0da258618b5cfc4de88191de80c
Spencer Hanson <spencer.hanson@swimlane.com>
2019-01-22 11:45:37 -0700

    Updated html, started on Spotify API integration

--- build/index.js
+++ build/index.js
@@ -2,11 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 // Create a new express application instance
 const app = express();
+let client_id = 'bc7420c5b36b4dbb9a94517195857c9f';
+let client_secret = fs.readFileSync('.secret', 'utf8').trim();
+let redirect_uri = 'http://localhost:8888';
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
-    // res.send('Hello!');
 });
 app.listen(8888, function () {
     console.log('Example app listening on port 8888!');
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import express = require('express');
 import path = require('path');
+import fs = require('fs');
 
 
 // Create a new express application instance
 const app: express.Application = express();
 
+
+let client_id: String = 'bc7420c5b36b4dbb9a94517195857c9f';
+let client_secret: String = fs.readFileSync('.secret', 'utf8').trim();
+let redirect_uri: String = 'http://localhost:8888';
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
-  // res.send('Hello!');
 });
 
 
